30d9ffa67e1893a8fd2c1675c5eb2df62869b328
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-12-16 18:06:26 +0530

    new form completed

--- src/components/NewProjectForm.jsx
+++ src/components/NewProjectForm.jsx
@@ -2,15 +2,37 @@ import React from "react";
 
 export default function NewProjectForm() {
   return (
-    <form className="">
-      <button className="">Cancel</button>
-      <button>Save</button>
-      <p>Title</p>
-      <input type="text" name="" id="" />
-      <p>Description</p>
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-      <p>Due Date</p>
-      <input type="date" name="" id="" />
+    <form className="pl-20 w-3/4">
+      <div className="h-8">
+        <button className="float-right bg-black text-white h-10 w-20 rounded-md ml-4">
+          Save
+        </button>
+        <button className="float-right h-10 w-20 font-medium ">Cancel</button>
+      </div>
+      <p className="uppercase mt-10 mb-1 font-semibold text-slate-600">Title</p>
+      <input
+        className="bg-gray-300 text-slate-700 w-full h-10 rounded-md focus:outline-none focus:border-b-4 focus:text-slate-700 p-1 focus:border-slate-700"
+        type="text"
+        name=""
+        id=""
+      />
+      <p className="uppercase text-slate-600 font-semibold mt-6 mb-1">
+        Description
+      </p>
+      <textarea
+        className="bg-gray-300 text-slate-700 w-full rounded-md focus:border-b-4 focus:outline-none p-1 focus:border-slate-700"
+        name=""
+        id=""
+        rows="4"
+      ></textarea>
+      <p className="uppercase text-slate-600 font-semibold mt-4">Due Date</p>
+      <input
+        className="bg-gray-300 mt-1 w-full h-10 rounded-md p-2 text-slate-700 
+        focus:outline-none focus:border-b-4 focus:border-slate-700"
+        type="date"
+        name=""
+        id=""
+      />
     </form>
   );
 }
