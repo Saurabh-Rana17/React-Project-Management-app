b2f0f68b02834e18f01a0e907c6e80f14e0120fb
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-12-18 14:04:39 +0530

    styling changed

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -5,14 +5,14 @@ export default function Empty({ handleOpenNewProject }) {
   return (
     <div className="flex flex-col items-center justify-center">
       <img className="h-20 w-20 m-8 " src={imgSrc} alt="image" />
-      <h1 className="text-xl font-bold text-amber-950 mb-6">
+      <h1 className="text-xl font-bold text-slate-950-950 mb-6">
         No Project Selected
       </h1>
       <h2 className="text-gray-500 mb-6 font-medium ">
         Select a Project or get started with a new one
       </h2>
       <button
-        className="text-gray-400 h-12 w-48 rounded-md mt-2  bg-amber-950 hover:bg-amber-900 hover:text-white"
+        className="text-gray-100 h-12 w-48 rounded-md mt-2  bg-slate-950 hover:bg-slate-600 hover:text-white "
         onClick={handleOpenNewProject}
       >
         Create a new Project
--- src/components/NewProjectForm.jsx
+++ src/components/NewProjectForm.jsx
@@ -1,14 +1,14 @@
 import React, { useRef } from "react";
 
 const NewProjectForm = function ({ setOpenNewForm, setIsEmpty, projectArray }) {
-  let title = useRef("");
-  let description = useRef("");
-  let dueDate = useRef("");
+  let title = useRef();
+  let description = useRef();
+  let dueDate = useRef();
 
   function handleSave() {
-    title = title.current.value;
-    description = description.current.value;
-    dueDate = dueDate.current.value;
+    title = title.current.value || "empty";
+    description = description.current.value || "empty";
+    dueDate = dueDate.current.value || "";
     projectArray.push({
       title,
       description,
@@ -18,6 +18,7 @@ const NewProjectForm = function ({ setOpenNewForm, setIsEmpty, projectArray }) {
     setIsEmpty(true);
 
     setOpenNewForm(false);
+    console.log(projectArray);
   }
 
   function handleCancel() {
@@ -30,7 +31,7 @@ const NewProjectForm = function ({ setOpenNewForm, setIsEmpty, projectArray }) {
       <div className="h-8">
         <button
           onClick={handleSave}
-          className="float-right bg-black text-white h-10 w-20 rounded-md ml-4 hover:bg-slate-500"
+          className="float-right bg-black text-white h-10 w-20 rounded-md ml-4 hover:bg-green-600 hover:text-white hover:outline-green-800"
         >
           Save
         </button>
@@ -47,7 +48,7 @@ const NewProjectForm = function ({ setOpenNewForm, setIsEmpty, projectArray }) {
       <input
         ref={title}
         required
-        className="bg-gray-300 text-slate-700 w-full h-10 rounded-md focus:outline-none focus:border-b-4 focus:text-slate-700 p-1 focus:border-slate-700"
+        className="bg-gray-300 text-slate-700 w-full h-10 rounded-md focus:outline-none focus:border-b-4 focus:text-slate-700 p-1 focus:border-slate-700 border-b-slate-400 border-b-4"
         type="text"
         name=""
         id=""
@@ -58,7 +59,7 @@ const NewProjectForm = function ({ setOpenNewForm, setIsEmpty, projectArray }) {
       <textarea
         required
         ref={description}
-        className="bg-gray-300 text-slate-700 w-full rounded-md focus:border-b-4 focus:outline-none p-1 focus:border-slate-700"
+        className="bg-gray-300 text-slate-700 w-full rounded-md focus:border-b-4 focus:outline-none p-1 focus:border-slate-700 border-b-slate-400 border-b-4"
         name=""
         id=""
         rows="4"
@@ -68,7 +69,7 @@ const NewProjectForm = function ({ setOpenNewForm, setIsEmpty, projectArray }) {
         required
         ref={dueDate}
         className="bg-gray-300 mt-1 w-full h-10 rounded-md p-2 text-slate-700 
-        focus:outline-none focus:border-b-4 focus:border-slate-700"
+        focus:outline-none focus:border-b-4 focus:border-slate-700 border-b-slate-400 border-b-4"
         type="date"
         name=""
         id=""
